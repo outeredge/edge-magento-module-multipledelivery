bc9c488307105f055e290cec0aa3c3197e8d4e55
Victor Seager <vic@outeredgeuk.com>
2015-04-09 12:24:08 +0100

    added files

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# edge-magento-multipledelivery
+magento-multipledelivery-module
+===============================
+
+Multiple Delivery Methods module for Magento
+
+
+Adds a delivery method in Magento configuration which allows the adding of multiple delivery methods using a json encoded string.
+
+
+**Available Options**
+  - enabled (boolean)
+  - title (string)
+  - description (string)
+  - price (float)
+  - cost (float)
+  - postcodes (array)
+  - postcode_condition (allow|restrict)
+
+
+**An example setup**
+
+```
+{
+    "nextday": {
+        "enabled": true,
+        "title": "Next Day",
+        "description": "Next day delivery",
+        "price": 5,
+        "postcodes": ["PO20","PO19"],
+        "postcode_condition": "restrict"
+    },
+    "nextdaybefore10": {
+        "enabled": true,
+        "title": "Next Day (before 10am)",
+        "description": "Receive your items before 10am",
+        "price": 10,
+        "postcodes": ["PO20","PO19"],
+        "postcode_condition": "allow"
+    }
+}
+```
---
+++ app/code/local/Edge/Multidelivery/Model/Carrier/Multidelivery.php
@@ -0,0 +1,87 @@
+<?php
+
+class Edge_Multidelivery_Model_Carrier_Multidelivery
+    extends Mage_Shipping_Model_Carrier_Abstract
+    implements Mage_Shipping_Model_Carrier_Interface
+{
+
+    protected $_code = 'multidelivery';
+    protected $_isFixed = true;
+
+    public function collectRates(Mage_Shipping_Model_Rate_Request $request)
+    {
+        if (!$this->getConfigFlag('active')) {
+            return false;
+        }
+
+        $defaults = array(
+            'enabled'       => false,
+            'title'         => 'Standard',
+            'description'   => '',
+            'price'         => 0,
+            'cost'          => 0
+        );
+
+        $result = Mage::getModel('shipping/rate_result');
+
+        $jsonMethods = $this->getConfigData('methods');
+        $methods = Mage::helper('core')->jsonDecode($jsonMethods);
+        foreach ($methods as $code=>$config){
+
+            // Merge the defaults with specific data to ensure all data is present
+            $config = array_merge($defaults, $config);
+
+            if(!$config['enabled']){
+                continue;
+            }
+
+            if(isset($config['postcodes']) && isset($config['postcode_condition'])){
+                if(!$this->allowMethodToPostcode($config['postcodes'], $config['postcode_condition'])){
+                    continue;
+                }
+            }
+
+            $method = Mage::getModel('shipping/rate_result_method');
+            $method->setCarrier('multidelivery')
+                   ->setCarrierTitle($config['title'])
+                   ->setMethod($code)
+                   ->setMethodTitle($config['title'])
+                   ->setMethodDescription($config['description'])
+                   ->setPrice($config['price'])
+                   ->setCost($config['cost']);
+
+            $result->append($method);
+        }
+
+        return $result;
+    }
+
+    protected function allowMethodToPostcode($postcodes, $condition)
+    {
+        $shippingPostcode = Mage::getSingleton('checkout/session')->getQuote()->getShippingAddress()->getData('postcode');
+        if ($shippingPostcode){
+            if($condition === 'allow'){
+                foreach ($postcodes as $postcode){
+                    if (strpos($shippingPostcode, $postcode) === 0){
+                        return true;
+                    }
+                }
+                return false;
+            }
+            elseif ($condition === 'restrict'){
+                foreach ($postcodes as $postcode){
+                    if (strpos($shippingPostcode, $postcode) === 0){
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function getAllowedMethods()
+    {
+        return array('multidelivery' => 'Multiple Delivery Methods');
+    }
+}
